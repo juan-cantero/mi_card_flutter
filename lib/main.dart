79d81ad8a5b36eb2f06c8b3b72e2ac28a759ed44
juan cantero <juan.cantero@outlook.com>
2022-06-28 17:29:18 -0300

    finish

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,52 @@ void main() {
   runApp(MyApp());
 }
 
+var myAvatar = CircleAvatar(
+  radius: 50.0,
+  backgroundImage: AssetImage('assets/images/me.jpg'),
+);
+
+var name = Text('Juan Cantero',
+    style:
+        TextStyle(fontSize: 40.0, color: Colors.white, fontFamily: 'Pacifico'));
+
+var title = Text(
+  'Flutter Developer'.toUpperCase(),
+  style: TextStyle(
+      fontSize: 20.0,
+      color: Colors.teal.shade100,
+      letterSpacing: 2.5,
+      fontWeight: FontWeight.bold),
+);
+
+var phone = Card(
+    color: Colors.white,
+    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+    child: ListTile(
+        leading: Icon(Icons.phone, color: Colors.teal),
+        title: Text(
+          '+4398234234',
+          style: TextStyle(fontSize: 20, color: Colors.teal.shade900),
+        )));
+
+var email = Card(
+  color: Colors.white,
+  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+  child: ListTile(
+    leading: Icon(Icons.email, color: Colors.teal),
+    title: Text(
+      'juan.cantero@outlook.com',
+      style: TextStyle(fontSize: 20, color: Colors.teal.shade900),
+    ),
+  ),
+);
+
+var line = SizedBox(
+  height: 20,
+  width: 150,
+  child: Divider(color: Colors.teal.shade100),
+);
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,7 +58,8 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            children: <Widget>[CircleAvatar()],
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[myAvatar, name, title, line, phone, email],
           ),
         ),
       ),
